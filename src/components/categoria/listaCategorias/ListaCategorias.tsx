8174e6ef0fa3080203f65239305c6bac24ba0fe3
Kelton <kelton.tinho23@gmail.com>
2024-07-07 09:42:23 -0300

    tentativa

--- src/components/categoria/listaCategorias/ListaCategorias.tsx
+++ src/components/categoria/listaCategorias/ListaCategorias.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import  { useEffect, useState } from 'react';
 import { Dna } from 'react-loader-spinner';
 import { useNavigate } from 'react-router-dom';
 import { buscar } from '../../../service/Service';
