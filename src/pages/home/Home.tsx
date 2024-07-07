9fca80e18d5d1e36d3f2adbcb604c8450095cad1
Kelton <kelton.tinho23@gmail.com>
2024-07-06 21:50:43 -0300

    removendo erros

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,30 +1,10 @@
-import React, { useState } from "react";
+
 import "./Home.css";
 import farmacia from "../../assets/Medicine-pana.svg";
 import benzetacil from "../../assets/R (4).jpg";
 
 function Home() {
-    const [showDescription, setShowDescription] = useState([false, false, false]);
-    const [cartItems, setCartItems] = useState([]);
-
-    const handleBuyClick = (event, index) => {
-        event.preventDefault(); // Prevenir o comportamento padrão do link
-        const updatedShowDescription = [...showDescription];
-        updatedShowDescription[index] = !updatedShowDescription[index];
-        setShowDescription(updatedShowDescription);
-    };
-
-    const handleCloseClick = (index) => {
-        const updatedShowDescription = [...showDescription];
-        updatedShowDescription[index] = false;
-        setShowDescription(updatedShowDescription);
-    };
-
-    const handleAddToCart = (item) => {
-        setCartItems([...cartItems, item]);
-        alert(`Item adicionado ao carrinho: ${item.name}`);
-    };
-
+    
     return (
         <>
             <div className="h-screen">
@@ -38,24 +18,23 @@ function Home() {
             <div className="partedois">
                 
                 <div className="compras">
-                    {[0, 1, 2].map((index) => (
-                        <div className={`imagem ${showDescription[index] ? 'show' : ''}`} key={index}>
+                        <div className="imagem">
                             <img src={benzetacil} alt="" />
                             <div className="compra">
-                                <a href="#" onClick={(e) => handleBuyClick(e, index)}>Compre</a>
+                                <a href="#">Compre</a>
                             </div>
                            
                             <div className="description">
                                 <p>Este é um medicamento de alta qualidade, recomendado por especialistas. Ideal para tratar diversas condições de saúde. Consulte seu médico para mais informações.</p>
                                 <hr />
-                                <button onClick={() => handleAddToCart({ id: index, name: `Benzetacil ${index + 1}` })}>77,00</button>
+                                <button >77,00</button>
                             </div>
-                            <button className={`close-button ${showDescription[index] ? 'show' : ''}`} onClick={() => handleCloseClick(index)}>Fechar</button>
+                            <button >Fechar</button>
                             <div className="icon">
                                 <p>Adicionar ao carrinho</p>
                             </div>
                         </div>
-                    ))}
+                    
                 </div>
             
              <h2>Promoção</h2>
